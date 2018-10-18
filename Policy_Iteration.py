4c34311f7620b6c3cff5238f3197a63b6608b67b
Tung.Cheng <tung.cheng.wu@gmail.com>
2018-10-18 23:47:31 +0800

    add greedy select action method, leaving main function to be completed.

--- Policy_Iteration.py
+++ Policy_Iteration.py
@@ -3,13 +3,13 @@ import numpy as np
 import time, os
 
 ## define function
-def IterativePolicyEvalution(func_value, prob_action, func_reward, trans_mat, gamma):
+def PolicyEvalution(func_value, best_action, func_reward, trans_mat, gamma):
     func_value_now = func_value.copy()
     for state in range(1,15):
-        prob_next_state = prob_action[state,:]*trans_mat[:,state,:]
+        next_state = trans_mat[:, state, best_action[state]]
         future_reward = func_reward + func_value_now*gamma
-        func_value[state] = np.sum(np.matmul(np.transpose(prob_next_state), future_reward))
-        delta = np.max(np.abs(func_value - func_value_now))
+        func_value[state] = np.sum(next_state*future_reward)
+    delta = np.max(np.abs(func_value - func_value_now))
     return func_value, delta
 
 def ShowValue(delta, theta, gamma, counter, func_value):
@@ -25,16 +25,23 @@ def ShowValue(delta, theta, gamma, counter, func_value):
     print(func_value.reshape(4,4))
     print('='*60)
 
-#def PolicyImprovement(func_value, prob_action, func_reward, trans_mat, gamma):
-#    prob_action_now = np.argmax(prob_action)
-#    for state in range(1,15):
-#        best_action_now = best_action.copy()
+def PolicyImprovement(func_value, best_action, prob_action, func_reward, trans_mat, gamma):
+    policy_stable = False
+    best_action_now = best_action.copy()
+    for state in range(1,15):
+        prob_next_state = prob_action[state]*trans_mat[:,state,:]
+        future_reward = func_reward + func_value*gamma
+        best_action[state] = np.argmax(np.matmul(np.transpose(prob_next_state), future_reward))
+    if np.all(best_action == best_action_now):
+        policy_stable = True
+    return best_action, policy_stable
 
 # main function
 def main():
     ## environment setting
     # action
-    ProbAction = np.zeros([16, 4])
+    BestAction = np.random.randint(0,4,16)
+    ProbAction = np.zeros([16,4])
     ProbAction[1:15,:] = 0.25
     # value function
     FuncValue = np.zeros(16)
@@ -53,10 +60,13 @@ def main():
 
     # iteration
     while delta > theta:
-        FuncValue, delta = IterativePolicyEvalution(FuncValue, ProbAction, FuncReward, T, gamma)
+        FuncValue, delta = PolicyEvalution(FuncValue, BestAction, FuncReward, T, gamma)
         ShowValue(delta, theta, gamma, counter, FuncValue)
         counter += 1
         time.sleep(1)
+
+    BestAction, PolicyStable = PolicyImprovement(FuncValue, BestAction, ProbAction, FuncReward, T, gamma)
+
 
 # execute
 if __name__ == '__main__':
