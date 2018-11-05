bfeb380aad51f5296ae8a2155fd297b9c60a3b3a
WU_GCP <tung.cheng.wu@gmail.com>
2018-11-05 07:19:48 +0000

    modified ploting

--- cliffwalk/plot.py
+++ cliffwalk/plot.py
@@ -7,15 +7,15 @@ q = np.load('q-episode-reward.npy')
 s = np.load('s-episode-reward.npy')
 
 plt.subplot(1,2,1)
-plt.plot(x, s, 'gray')
+plt.plot(x, s, 'blue')
 plt.title('SARSA')
 plt.xlabel('Episodes')
 plt.ylabel('Reward per Episode')
 plt.ylim(-500, 0)
 
 plt.subplot(1,2,2)
-plt.plot(x, q, 'black')
-plt.title('Q-Learn')
+plt.plot(x, q, 'orange')
+plt.title('Q-Learning')
 plt.xlabel('Episodes')
 plt.ylim(-500, 0)
 
